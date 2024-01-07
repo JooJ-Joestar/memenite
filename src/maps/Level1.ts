d181d6377d115baf25072f3d1457f84ac34cbb90
JooJ <jorge@jooj.dev>
2024-01-07 15:26:53 -0300

    feat: added colliders.

--- src/maps/Level1.ts
+++ src/maps/Level1.ts
@@ -4,13 +4,37 @@ import type {PlayerAttributes} from '../types/PlayerAttributes';
 
 export class Level1 {
     private scene: BABYLON.Scene;
+    private colliders: BABYLON.AbstractMesh[] = [];
 
     constructor (scene: BABYLON.Scene) {
+        let _colliders: any = [];
 
         // The first parameter can be used to specify which mesh to import. Here we import all meshes
         BABYLON.SceneLoader.ImportMesh("", "/assets/maps/level-1/", "level-1.babylon", scene, function (newMeshes) {
             // Set the target of the camera to the first imported mesh
             // camera.target = newMeshes[0];
+        });
+
+        let import_mesh = BABYLON.SceneLoader.ImportMeshAsync("", "/assets/maps/level-1/", "level-1-colliders.babylon", scene, function (newMeshes) {
+        });
+
+        async function afterMeshIsImported (colliders: BABYLON.AbstractMesh[]) {
+            console.log('Calling');
+            const result = await import_mesh;
+            for (let i in result.meshes) {
+                if (result.meshes[i].name.indexOf("collider") == -1) return;
+                colliders.push(result.meshes[i]);
+            }
+        }
+        afterMeshIsImported(this.colliders);
+
+        window.addEventListener("keydown", (ev) => {
+            //Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'C') {
+                for (let i in this.colliders) {
+                    this.colliders[i].visibility = Math.abs(Math.abs(this.colliders[i].visibility) - 1);
+                }
+            }
         });
 
         var kek_options: PlayerAttributes = {
