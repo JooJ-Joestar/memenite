4c9646194ab76d8df9742645655c26e4576c623b
JooJ <jorge@jooj.dev>
2024-01-16 00:38:25 -0300

    feat: Same thing as last commit.

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,3 @@ window.addEventListener('DOMContentLoaded', () => {
     let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
     let app = new App(canvas);
     app.run();
-});
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-//     let app = new App(canvas);
-//     // app.run();
-// });
