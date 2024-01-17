0e02a463652c46462797ba1f69d7251d85ed676c
JooJ <jorge@jooj.dev>
2024-01-17 01:45:54 -0300

    feat: Adding commands to elements.

--- src/models/Hud.ts
+++ src/models/Hud.ts
@@ -20,6 +20,16 @@ export class Hud {
 
         let import_hud = advancedTexture.parseFromSnippetAsync("#0QWKF2#4");
         async function afterHudIsImported() {
+            const result = await import_hud;
+            console.log(advancedTexture.getDescendants());
+            const field_nickname: any = advancedTexture.getControlByName("nickname");
+            const btn_ready: any = advancedTexture.getControlByName("ready");
+
+            btn_ready.onPointerUpObservable.add(function() {
+                console.log(field_nickname.text);
+                field_nickname.isVisible = false;
+                btn_ready.isVisible = false;
+            });
         }
         afterHudIsImported();
     }
