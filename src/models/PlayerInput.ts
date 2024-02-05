959c8ba33841d43126db6a725c83bedf456d0928
JooJ <jorge@jooj.dev>
2024-02-04 23:33:13 -0300

    feat: Keyboard controls were readded. It is now possible to use both keyboard and mobile controls at the same time.

--- src/models/PlayerInput.ts
+++ src/models/PlayerInput.ts
@@ -1,4 +1,5 @@
 import * as BABYLON from '@babylonjs/core'
+import { BabylonApp } from '../BabylonApp';
 import { Hud } from './Hud';
 
 export class PlayerInput {
@@ -12,9 +13,7 @@ export class PlayerInput {
     public horizontalAxis: number = 0;
     public verticalAxis: number = 0;
 
-    //jumping and dashing
-    public jumpKeyDown: boolean = false;
-    public dashing: boolean = false;
+    public controls_pressed: boolean = false;
 
     //Mobile Input trackers
     private ui: Hud;
@@ -37,7 +36,7 @@ export class PlayerInput {
 
         //add to the scene an observable that calls updateFromKeyboard before rendering
         scene.onBeforeRenderObservable.add(() => {
-            // this.updateFromKeyboard();
+            this.updateFromKeyboard();
             this.updateFromUi();
         });
 
@@ -50,17 +49,19 @@ export class PlayerInput {
     // Keyboard controls & Mobile controls
     //handles what is done when keys are pressed or if on mobile, when buttons are pressed
     private updateFromKeyboard(): void {
+        this.controls_pressed = false;
 
         //forward - backwards movement
         if ((this.inputMap["ArrowUp"]) /*&& !this.ui.gamePaused*/) {
             // console.log("ArrowUp");
             this.verticalAxis = 1;
             this.vertical = BABYLON.Scalar.Lerp(this.vertical, 1, 0.2);
-
+            this.controls_pressed = true;
         } else if ((this.inputMap["ArrowDown"]) /*&& !this.ui.gamePaused*/) {
             // console.log("ArrowDown");
             this.vertical = BABYLON.Scalar.Lerp(this.vertical, -1, 0.2);
             this.verticalAxis = -1;
+            this.controls_pressed = true;
         } else {
             // console.log("None");
             this.vertical = 0;
@@ -74,26 +75,40 @@ export class PlayerInput {
             //taking current horizontal and how long you hold, will go up to -1(all the way left)
             this.horizontal = BABYLON.Scalar.Lerp(this.horizontal, -1, 0.2);
             this.horizontalAxis = -1;
+            this.controls_pressed = true;
 
         } else if ((this.inputMap["ArrowRight"]) /*&& !this.ui.gamePaused*/) {
             // console.log("ArrowRight");
             this.horizontal = BABYLON.Scalar.Lerp(this.horizontal, 1, 0.2);
             this.horizontalAxis = 1;
+            this.controls_pressed = true;
         }
         else {
             this.horizontal = 0;
             this.horizontalAxis = 0;
         }
+
+        if ((this.inputMap[" "]) /*&& !this.ui.gamePaused*/) {
+            let player = BabylonApp.singleton().room.get_current_player();
+            player[player.weapon_selected].fire();
+            this.controls_pressed = true;
+        }
     }
 
     private updateFromUi () {
+        if (this.controls_pressed === true) return false;
+
         if (this.ui.movement_x != 0) {
             // console.log(this.ui.movement_x);
             this.horizontal = this.ui.movement_x / 130;
+            this.controls_pressed = true;
         } else this.horizontal = 0;
         if (this.ui.movement_z != 0) {
             // console.log(this.ui.movement_z);
             this.vertical = (this.ui.movement_z / 130) * -1;
+            this.controls_pressed = true;
         } else this.vertical = 0;
+
+        return true;
     }
 }
